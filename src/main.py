0754aaccd70f932d21b7b532b44782be1a01efcf
Alexander Liao <alex.yj.liao@gmail.com>
2021-07-21 15:50:03 -0400

    bot go brrr

---
+++ src/main.py
@@ -0,0 +1,66 @@
+import asyncio, discord, json, re
+
+with open("../configurations/paimon.json", "r") as f:
+    config = json.load(f)
+
+def save():
+    with open("../configurations/paimon.json", "w") as f:
+        json.dump(config, f, indent = 4)
+
+def safe_eval(regex):
+    return eval('"' + regex.replace('"', '\\"') + '"')
+
+class DiscordClient(discord.Client):
+    def __init__(self):
+        discord.Client.__init__(self, intents = discord.Intents.all())
+
+    async def on_ready(self):
+        print("PAIMON has started.")
+
+    async def on_message(self, message):
+        if message == "FORCE KILL":
+            if message.author.id == self.user.id:
+                await message.channel.send("Goodbye, cruel world.")
+            exit(0)
+        if message.author.id == self.user.id:
+            return
+        lines = message.content.split("\n")
+        if len(lines) == 4 and lines[0] == "WHEN" and lines[2] in ["SEND", "SEND FN MATCH", "SEND FN MSG", "SEND FN BOTH"]:
+            config["regexes"].append([safe_eval("^" + lines[1] + "$"), lines[3], message.author.id in config["trusted"], ["SEND", "SEND FN MATCH", "SEND FN MSG", "SEND FN BOTH"].index(lines[2]), message.guild.id])
+            save()
+            await message.channel.send(f"Added message rule #{len(config['regexes'])}. Enable/disable it with `ENABLE #` / `DISABLE #`." + " Since you are not a trusted user, your rule is currently disabled." * (message.author.id not in config["trusted"]))
+        elif message.content == "RULE LIST":
+            outputs = []
+            for index, (rule, replacement, enabled, type, guild) in enumerate(config["regexes"]):
+                outputs.append(f"RULE #{index + 1} ({['dis', 'en'][enabled]}abled):" + "\n" + f"match   : {repr(rule)}" + "\n" + f"replace : {repr(replacement) if type == 0 else replacement}" + "\n" + "type    : " + ["regex substitution", "regex match function", "discord message function", "match + message function"][type] + "\n" + "guild   : " + self.get_guild(guild).name)
+            await message.channel.send("```\n" + "\n\n".join(outputs) + "\n```")
+        elif re.match(r"(EN|DIS)ABLE \d+", message.content):
+            if a == "ENABLE" and message.author.id not in config["trusted"]:
+                await message.reply("Only trusted users may enable regex rules.")
+            else:
+                a, b = message.content.split()
+                b = int(b)
+                a = a == "ENABLE"
+                nr = len(config["regexes"])
+                if 0 < b <= nr:
+                    config["regexes"][b - 1][2] = a
+                    save()
+                    await message.channel.send(f"Rule #{b} has been {'en' if a else 'dis'}abled.")
+                else:
+                    await message.channel.send(f"There is no rule with that ID. There {['are', 'is'][nr == 1]} currently {nr} rule{'s' * (nr != 1)}.")
+        for rule, replacement, enabled, type, guild in config["regexes"]:
+            if not enabled: continue
+            if message.guild.id != guild: continue
+            if type == 0:
+                r = safe_eval(replacement)
+            elif type == 1:
+                r = eval(replacement)
+            elif type == 2:
+                r = lambda match: eval(replacement)(message)
+            elif type == 3:
+                r = lambda match: eval(replacement)(message, match)
+            if re.match(rule, message.content):
+                await message.channel.send(re.sub(rule, r, message.content))
+
+client = DiscordClient()
+client.run(config["discord-token"])
