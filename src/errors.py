4c13c9de8fedbdf950c4fd5c3b5a78a178537db5
Alexander Liao <alex.yj.liao@gmail.com>
2021-07-21 13:24:07 -0400

    restart

--- src/errors.py
+++
@@ -1,8 +0,0 @@
-class BotError(RuntimeError):
-  def __init__(self, content, **kwargs):
-    self.content = content
-    self.kwargs = kwargs
-
-class PublicBotError(RuntimeError):
-  def __init__(self, message):
-    self.message = message
